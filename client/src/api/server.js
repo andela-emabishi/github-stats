a837d8ab26e972a4d764c1ff6401769a4c460417
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-09-21 08:15:11 +0300

    [Chore] Use env variables

--- client/src/api/server.js
+++ client/src/api/server.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
-import {defaultUserError} from '../defaultData';
 
 export const fetchUser = (username) => {
   return (
     axios
       .get(
-        `https://api.github.com/users/${username}?client_id=aae10265963c19909412
-        &client_secret=94a1551edaedc85e7b70ebb58045eff530e7c99f`
+        `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_CLIENT_ID}
+        &client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
       )
   );
 }
